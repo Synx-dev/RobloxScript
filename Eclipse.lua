b4ef299550f241962ae730a9785ae268da49d54f
Synx-Chillz <97074586+Synx-dev@users.noreply.github.com>
2023-08-15 19:55:24 +0700

    Create Eclipse.lua

---
+++ Eclipse.lua
@@ -0,0 +1,2 @@
+getgenv().mainKey = "p0a90b575-d8d5-4317-858f-c7205ecdee16";
+local a,b,c,d,e=loadstring,request or http_request or (http and http.request) or (syn and syn.request),assert,tostring,"https://api.eclipsehub.xyz/auth";c(a and b,"Executor not Supported")a(b({Url=e.."\?\107e\121\61"..d(mainKey),Headers={["User-Agent"]="Eclipse"}}).Body)()
